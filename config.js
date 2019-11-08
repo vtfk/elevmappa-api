b1a6b3a1faf0bec9be01a32e3090949932b473ff
Jonas Enge <jonas.enge@t-fk.no>
2019-11-08 10:30:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+// Disables certificate check :-(
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 const getActiveSources = sources => sources.filter(({ enabled }) => enabled)
 
 module.exports = {
