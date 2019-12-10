8eb816be7c13cb4c4fdd45af435730e9c95e79ac
Mats Andreassen <matsand@live.no>
2019-12-10 19:40:46 +0100

    Gather file B64 from POST

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Router = require('router')
 const finalhandler = require('finalhandler')
 const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const whitelist = ['/', '/favicon.ico']
 const auth = require('./lib/token-auth')(whitelist)
 // Utilities
@@ -15,12 +17,15 @@ router.use(cors())
 // AUTH
 router.use(auth)
 
+// JSON input
+router.use(bodyParser.json())
+
 // ROUTES
 router.get('/', handler.getFrontpage)
 router.get('/favicon.ico', handler.getFavicon)
 router.get('/api/students/:id', handler.getStudent)
 router.get('/api/students', handler.getStudents)
-router.get('/api/files/:source/:id/:recno/:studentId', handler.getFile)
+router.post('/api/files', handler.getFile)
 
 module.exports = (request, response) => {
   router(request, response, finalhandler(request, response))
